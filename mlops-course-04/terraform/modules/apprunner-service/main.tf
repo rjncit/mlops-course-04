95dffcc344ae4e136ff3162b7bfa960f2522f08d
Rajan Joshi <rajanjoshi@Rajans-MacBook-Pro.local>
2025-06-08 23:48:33 +0200

    changes

--- mlops-course-04/terraform/modules/apprunner-service/main.tf
+++ mlops-course-04/terraform/modules/apprunner-service/main.tf
@@ -7,14 +7,9 @@ resource "aws_apprunner_service" "ars" {
     }
 
     image_repository {
-      image_identifier = (
-        contains(var.source_configuration.image_repository.image_identifier, ":")
-        ? var.source_configuration.image_repository.image_identifier
-        : "${var.source_configuration.image_repository.image_identifier}:latest"
-      )
-
+      image_identifier = var.source_configuration.image_repository.image_identifier
       image_repository_type = var.source_configuration.image_repository.image_repository_type
-
+      
       image_configuration {
         port = var.source_configuration.image_repository.image_configuration.port
       }
@@ -23,5 +18,56 @@ resource "aws_apprunner_service" "ars" {
     auto_deployments_enabled = var.source_configuration.auto_deployments_enabled
   }
 
+  health_check_configuration {
+    protocol            = "HTTP"
+    path                = "/"
+    interval            = 10
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
   tags = var.tags
+}
+
+resource "aws_iam_role" "iamr" {
+  name = "${local.service_name}-iam-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "build.apprunner.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "apprunner_ecr_access" {
+  role       = aws_iam_role.iamr.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
+
+resource "aws_iam_role_policy" "additional_ecr_permissions" {
+  name = "additional-ecr-permissions"
+  role = aws_iam_role.iamr.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetAuthorizationToken"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
